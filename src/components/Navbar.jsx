e1079972052ea37f4604bbfa6bbead1cf6f0077e
NoelFrey <leo.agzamov@gmail.com>
2022-05-03 00:51:19 +0600

    still wont

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,11 +9,11 @@ class NavBar extends Component {
         <nav>
           <NavLink to={'/'}>Главная</NavLink>
           <NavLink to={'./pages/budget'}>Бюджетные</NavLink>
-          <NavLink to={'./pages//middle'}>Среднего класса</NavLink>
-          <NavLink to={'./pages//multi'}>Мультимедийные</NavLink>
-          <NavLink to={'./pages//gamers'}>Игровые</NavLink>
-          <NavLink to={'./pages//style'}>Имиджевые</NavLink>
-          <NavLink to={'./pages//sensor'}>С сенсорным дисплеем</NavLink>
+          <NavLink to={'./pages/middle'}>Среднего класса</NavLink>
+          <NavLink to={'./pages/multi'}>Мультимедийные</NavLink>
+          <NavLink to={'./pages/gamers'}>Игровые</NavLink>
+          <NavLink to={'./pages/style'}>Имиджевые</NavLink>
+          <NavLink to={'./pages/sensor'}>С сенсорным дисплеем</NavLink>
         </nav>
       </BrowserRouter>
     );
